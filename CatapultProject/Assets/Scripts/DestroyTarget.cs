1bc1c3c3d27be923edb323c60702dc45c1e24be7
StefBasGusFlus <stefanbasarin@gmail.com>
2023-01-25 14:32:42 +0300

    Add music

--- CatapultProject/Assets/Scripts/DestroyTarget.cs
+++ CatapultProject/Assets/Scripts/DestroyTarget.cs
@@ -6,12 +6,14 @@ using UnityEngine.UI;
 public class DestroyTarget : MonoBehaviour
 {
     [SerializeField] private Text _win;
+    [SerializeField] private AudioSource _audioSource;
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Attack")
         {
             Destroy(gameObject);
             _win.text = "Выиграл!";
+            _audioSource.Play();
         }
            
     }
