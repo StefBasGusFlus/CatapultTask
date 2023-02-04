9bd9dcb8498c216069afcd16841e41bbc11364d4
StefBasGusFlus <stefanbasarin@gmail.com>
2023-02-04 17:27:41 +0300

    Codestyle

--- CatapultProject/Assets/Scripts/DestroyTarget.cs
+++ CatapultProject/Assets/Scripts/DestroyTarget.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,14 +5,16 @@ public class DestroyTarget : MonoBehaviour
 {
     [SerializeField] private Text _win;
     [SerializeField] private AudioSource _audioSource;
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Attack")
         {
             Destroy(gameObject);
+
             _win.text = "Выиграл!";
+
             _audioSource.Play();
         }
-           
     }
 }
--- CatapultProject/Assets/Scripts/SpawnPlayer.cs
+++ CatapultProject/Assets/Scripts/SpawnPlayer.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,11 +5,13 @@ public class SpawnPlayer : MonoBehaviour
 {
     [SerializeField] private GameObject _player;
     [SerializeField] private Text _win;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(_player, transform.position, transform.rotation);
+
             _win.text = "";
         }
     }
